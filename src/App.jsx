73a439523d9a9b09dfd896574f75b663d084ed56
Francisco-Villanueva <franciscovillanuevaj99@gmail.com>
2024-10-29 20:37:43 -0300

    feat ( UI ): created landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,14 @@
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <h1>Aplicacion en desarrollo</h1>
+
+      <br />
+      <hr />
+
+      <p>LANDGIN PAGE</p>
     </>
   );
 }
