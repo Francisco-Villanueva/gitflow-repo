68fd9bcb19884a833924bd447a1f7365c5cc8d86
Francisco-Villanueva <franciscovillanuevaj99@gmail.com>
2024-10-29 20:20:22 -0300

    este commit tiene que fallar

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Aplicacion en develop 22!</h1>
+      <h1>Aplicacion en desarrollo</h1>
     </>
   );
 }
